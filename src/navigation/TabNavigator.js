ba95ed39a0972cbf484a0063b69b82c2009bf69e
NurgaliyevS <nurgaliyev000@gmail.com>
2022-08-18 21:12:48 +0600

    Add package and icon navigation

--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -5,15 +5,46 @@ import HomeScreen from '../screen/HomeScreen';
 import FavoriteScreen from '../screen/FavoriteScreen';
 import CartScreen from '../screen/CartScreen';
 
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 const Tab = createBottomTabNavigator();
 
 function TabNavigator() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name='Home' component={HomeScreen} />
-        <Tab.Screen name='Favorite' component={FavoriteScreen} />
-        <Tab.Screen name='Cart' component={CartScreen} />
+      <Tab.Navigator
+        screenOptions={{
+          tabBarShowLabel: false,
+          headerShown: false,
+        }}
+      >
+        <Tab.Screen
+          name='Home'
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name='home-outline' color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name='Favorite'
+          component={FavoriteScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name='home-outline' color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name='Cart'
+          component={CartScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name='home-outline' color={color} size={size} />
+            ),
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
