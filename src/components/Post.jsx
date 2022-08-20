081e3d9b51fba588cb2036ece1086040e0b54eb6
NurgaliyevS <nurgaliyev000@gmail.com>
2022-08-20 09:43:50 +0600

    Add ScrollView

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,9 +1,12 @@
-import { View } from 'react-native';
+import { ScrollView, View } from 'react-native';
 import styled from 'styled-components/native';
 
 const PostView = styled.View`
   flex-direction: row;
   padding: 15px;
+`;
+
+const PostContainer = styled.View`
   border-bottom-width: 1px;
   border-bottom-color: rbga(0, 0, 0, 0.1);
   border-bottom-style: solid;
@@ -12,26 +15,25 @@ const PostView = styled.View`
 const PostImage = styled.Image`
   width: 120px;
   height: 120px;
+  margin: 0 15px 0 0;
   border-radius: 12px;
-  margin-right: 12px;
 `;
 
 const PostTitle = styled.Text`
-  flex-direction: row;
+  margin-top: 20px;
   padding: 15px;
   font-size: 17px;
   font-weight: 600;
 `;
 
-const PostDate = styled.Text`
-  font-size: 12px;
-  color: rgba(0, 0, 0, 0.4);
-  margin-top: 2px;
+const PostDescription = styled.Text`
+  flex: 2;
 `;
 
-const PostDetails = styled.View`
-  flex: 1;
-  justify-content: center;
+const PostDescriptionUnderImg = styled.Text`
+  padding-right: 15px;
+  padding-bottom: 15px;
+  padding-left: 15px;
 `;
 
 const truncateTitle = (str) => {
@@ -44,26 +46,58 @@ const truncateTitle = (str) => {
 
 export default function Post() {
   return (
-    <View>
-      <PostTitle>
-        {truncateTitle('История строительства метрополитена')}
-      </PostTitle>
-      <PostView>
-        <PostImage source={require('../../assets/Metro-beginning.jpg')} />
-        <PostDetails>
-          В 1988 году в Алматы началось активное строительство метрополитена. Но
-          распад СССР в 1991 году и последовавший за ним разрыв хозяйственных и
-          экономических связей не позволил в полной мере осуществить
-          строительство метрополитена г. Алматы. Многие вопросы остались
-          нерешенными из-за ликвидации ряда министерств и ведомств. И это стало
-          причиной значительного отставания в сроках строительства метро. 1990-е
-          годы явились периодом создания собственной материально-технической и
-          научной базы метростроения. Несмотря на минимальное финансирование,
-          продолжалась проходка перегонных и эскалаторных станций, подходных
-          выработок к станциям глубокого заложения, а главное, были сохранены
-          опытные кадры инженеров и рабочих.
-        </PostDetails>
-      </PostView>
-    </View>
+    <ScrollView>
+      <View>
+        <PostTitle>
+          {truncateTitle('История строительства метрополитена')}
+        </PostTitle>
+        <PostContainer>
+          <PostView>
+            <PostImage source={require('../../assets/Metro-beginning.jpg')} />
+            <PostDescription>
+              В 1988 году в Алматы началось активное строительство
+              метрополитена. Но распад СССР в 1991 году и последовавший за ним
+              разрыв хозяйственных и экономических связей не позволил в полной
+              мере осуществить строительство метрополитена г. Алматы.
+            </PostDescription>
+          </PostView>
+          <PostDescriptionUnderImg>
+            Многие вопросы остались нерешенными из-за ликвидации ряда
+            министерств и ведомств. И это стало причиной значительного
+            отставания в сроках строительства метро. 1990-е годы явились
+            периодом создания собственной материально-технической и научной базы
+            метростроения. Несмотря на минимальное финансирование, продолжалась
+            проходка перегонных и эскалаторных станций, подходных выработок к
+            станциям глубокого заложения, а главное, были сохранены опытные
+            кадры инженеров и рабочих.
+          </PostDescriptionUnderImg>
+        </PostContainer>
+
+        <PostTitle>
+          {truncateTitle('История строительства метрополитена')}
+        </PostTitle>
+        <PostContainer>
+          <PostView>
+            <PostImage source={require('../../assets/Metro-beginning.jpg')} />
+            <PostDescription>
+              В 1988 году в Алматы началось активное строительство
+              метрополитена. Но распад СССР в 1991 году и последовавший за ним
+              разрыв хозяйственных и экономических связей не позволил в полной
+              мере осуществить строительство метрополитена г. Алматы.
+            </PostDescription>
+          </PostView>
+          <PostDescriptionUnderImg>
+            Многие вопросы остались нерешенными из-за ликвидации ряда
+            министерств и ведомств. И это стало причиной значительного
+            отставания в сроках строительства метро. 1990-е годы явились
+            периодом создания собственной материально-технической и научной базы
+            метростроения. Несмотря на минимальное финансирование, продолжалась
+            проходка перегонных и эскалаторных станций, подходных выработок к
+            станциям глубокого заложения, а главное, были сохранены опытные
+            кадры инженеров и рабочих.
+          </PostDescriptionUnderImg>
+        </PostContainer>
+      </View>
+    </ScrollView>
   );
 }
--- src/screen/HistoryScreen.js
+++ src/screen/HistoryScreen.js
@@ -7,28 +7,6 @@ function HistoryScreen() {
   return (
     <View>
       <Post />
-      {/* <FlatList
-        refreshControl={
-          <RefreshControl />
-        }
-        data={items}
-        renderItem={({ item }) => (
-          <TouchableOpacity
-            onPress={({}) => {
-              navigation.navigate('FullPost', {
-                id: item?.id,
-                title: item?.title,
-              });
-            }}
-          >
-            <Post
-              title={item.title}
-              createdAt={item.createdAt}
-              imageUrl={item.imageUrl}
-            />
-          </TouchableOpacity>
-        )}
-      /> */}
     </View>
   );
 }
