f88ca8af6e40e8946ec49604ca941d633d3330f1
NurgaliyevS <nurgaliyev000@gmail.com>
2022-08-21 19:54:01 +0600

    moved to station

--- src/components/StationTimetable.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { Text } from 'react-native';
-
-function StationTimetable({ navigation, route }) {
-  return <Text>This is {route.params.name}'s profile</Text>;
-}
-
-export default StationTimetable;
