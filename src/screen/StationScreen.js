01b87454c3a041e24bbd01c4ba4a1e3afca46f9f
NurgaliyevS <nurgaliyev000@gmail.com>
2022-08-21 11:40:41 +0600

    StationScreen add screen

---
+++ src/screen/StationScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { View, Text } from 'react-native';
+import Station from '../components/Station';
+
+function StationScreen() {
+  return (
+    <View>
+      <Station />
+    </View>
+  );
+}
+
+export default StationScreen;
