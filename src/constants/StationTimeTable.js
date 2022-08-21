861e45acb77ccbc4dd73e02de1ac0f00e5d099d1
NurgaliyevS <nurgaliyev000@gmail.com>
2022-08-21 19:54:41 +0600

    add constants

---
+++ src/constants/StationTimeTable.js
@@ -0,0 +1,18 @@
+export const StationTimeTable = {
+  Raimbek: {
+    workingDays: {
+      0: '06:22:00',
+      1: '06:33:00',
+    },
+    weekends: {
+      0: '06:22:15',
+    },
+  },
+  zhibekToRaimbek: {
+    weekends: {
+      0: '06:30:25',
+      1: '06:45:25',
+      2: '07:01:25',
+    },
+  },
+};
