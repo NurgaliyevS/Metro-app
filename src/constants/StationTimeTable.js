4aea06fe6f5edb9bb1200d27d199971f6254b0dd
NurgaliyevS <snurgaliev@cic.kz>
2022-08-22 09:25:09 +0600

    upd constants

--- src/constants/StationTimeTable.js
+++ src/constants/StationTimeTable.js
@@ -1,5 +1,5 @@
 export const StationTimeTable = {
-  Raimbek: {
+  raimbekToZhibek: {
     workingDays: {
       0: '06:22:00',
       1: '06:33:00',
@@ -8,11 +8,103 @@ export const StationTimeTable = {
       0: '06:22:15',
     },
   },
+
   zhibekToRaimbek: {
+    workingDays: {},
     weekends: {
       0: '06:30:25',
       1: '06:45:25',
       2: '07:01:25',
     },
+  },
+
+  zhibekToAlmaly: {
+    workingDays: {},
+    weekends: {},
+  },
+
+  almalyToZhibek: {
+    workingDays: {},
+    weekends: {},
+  },
+
+  almalyToAbai: {
+    workingDays: {},
+    weekends: {},
+  },
+
+  abaiToAlmaly: {
+    workingDays: {},
+    weekends: {},
+  },
+
+  abaiToBaikonur: {
+    workingDays: {},
+    weekends: {},
+  },
+
+  baikonurToAbai: {
+    workingDays: {},
+    weekends: {},
+  },
+
+  baikonurToTeatrAuezov: {
+    workingDays: {},
+    weekends: {},
+  },
+
+  teatrAuezovToBaikonur: {
+    workingDays: {},
+    weekends: {},
+  },
+
+  teatrAuezovToAlatau: {
+    workingDays: {},
+    weekends: {},
+  },
+
+  alatauToTeatrAuezov: {
+    workingDays: {},
+    weekends: {},
+  },
+
+  alatauToSairan: {
+    workingDays: {},
+    weekends: {},
+  },
+
+  sairanToAlatau: {
+    workingDays: {},
+    weekends: {},
+  },
+
+  sairanToMoscow: {
+    workingDays: {},
+    weekends: {},
+  },
+
+  moscowToSairan: {
+    workingDays: {},
+    weekends: {},
+  },
+
+  moscowToSaryarka: {
+    workingDays: {},
+    weekends: {},
+  },
+
+  saryarkaToMoscow: {
+    workingDays: {},
+    weekends: {},
+  },
+
+  saryarkaToMomushuly: {
+    workingDays: {},
+    weekends: {},
+  },
+
+  momushulyToSaryarka: {
+    workingDays: {},
+    weekends: {},
   },
 };
