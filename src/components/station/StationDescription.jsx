56933c1a6c53685300cf87d61ff24880c2b1d5d1
NurgaliyevS <snurgaliev@cic.kz>
2022-08-23 19:08:05 +0600

    add component

---
+++ src/components/station/StationDescription.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { View, Text } from 'react-native';
+
+function StationDescription({ navigate }) {
+  return (
+    <View style={{ flex: 1, textAlign: 'center' }}>
+      <Text>Hello world</Text>
+    </View>
+  );
+}
+
+export default StationDescription;
