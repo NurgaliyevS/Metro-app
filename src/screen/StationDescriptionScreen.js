4e649c7443f8b01d6ce7a180c144b38488557ab8
NurgaliyevS <snurgaliev@cic.kz>
2022-08-23 19:09:03 +0600

    StationDescriptionScreen add screen

---
+++ src/screen/StationDescriptionScreen.js
@@ -0,0 +1,102 @@
+import React from 'react';
+
+import { View, Text, Select } from 'react-native';
+import styled from 'styled-components/native';
+import StationList from '../components/commonFIelds/StationList';
+import StationDescription from '../components/station/StationDescription';
+
+// CTRL + SHIFT + L
+// rsf
+
+const StationDescriptionView = styled.View`
+  flex: 1;
+  padding: 10px;
+`;
+
+const StationDescriptionContainerTitle = styled.View`
+  align-items: center;
+`;
+
+const StationDescriptionTitle = styled.Text`
+  padding-top: 20px;
+  font-size: 17px;
+  text-align: 'center';
+  font-weigth: bold;
+  padding-bottom: 30px;
+`;
+
+function StationDescriptionScreen({ navigation }) {
+  function navigateToStationDescription(stationName) {
+    switch (stationName.toLowerCase()) {
+      case 'райымбек батыра':
+        navigation.navigate('StationDescriptionComponent', {
+          name: 'Райымбек батыра',
+        });
+        break;
+      case 'жибек жолы':
+        navigation.navigate('StationDescriptionComponent', {
+          name: 'Жибек жолы',
+        });
+        break;
+      case 'алмалы':
+        navigation.navigate('StationDescriptionComponent', {
+          name: 'Алмалы',
+        });
+        break;
+      case 'абая':
+        navigation.navigate('StationDescriptionComponent', {
+          name: 'Абая',
+        });
+        break;
+      case 'байконыр':
+        navigation.navigate('StationDescriptionComponent', {
+          name: 'Байконыр',
+        });
+        break;
+      case 'театр имени мухтара ауэзова':
+        navigation.navigate('StationDescriptionComponent', {
+          name: 'Театр имени Мухтара Ауэзова',
+        });
+        break;
+      case 'алатау':
+        navigation.navigate('StationDescriptionComponent', {
+          name: 'Алатау',
+        });
+        break;
+      case 'сайран':
+        navigation.navigate('StationDescriptionComponent', {
+          name: 'Сайран',
+        });
+        break;
+      case 'москва':
+        navigation.navigate('StationDescriptionComponent', {
+          name: 'Москва',
+        });
+        break;
+      case 'сарыарка':
+        navigation.navigate('StationDescriptionComponent', {
+          name: 'Сарыарка',
+        });
+        break;
+      case 'б.момышулы':
+        navigation.navigate('StationDescriptionComponent', {
+          name: 'Б.Момышулы',
+        });
+        break;
+    }
+  }
+
+  return (
+    <StationDescriptionView>
+      <StationDescriptionContainerTitle>
+        <StationDescriptionTitle>Описание станций</StationDescriptionTitle>
+      </StationDescriptionContainerTitle>
+
+      <StationList
+        navigateToStationDescription={navigateToStationDescription}
+      />
+    </StationDescriptionView>
+  );
+}
+
+export default StationDescriptionScreen;
