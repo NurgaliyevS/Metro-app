5278ab4cb8463d97db76e8f7409e58a3c9122e08
NurgaliyevS <snurgaliev@cic.kz>
2022-08-23 19:07:57 +0600

    moved logic from station to station list

--- src/components/station/Station.jsx
+++ src/components/station/Station.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, ScrollView, Pressable } from 'react-native';
 import styled from 'styled-components/native';
+import StationList from '../commonFIelds/StationList';
 
 const StationView = styled.View`
   padding: 15px;
@@ -16,142 +17,68 @@ const StationTitle = styled.Text`
 `;
 
 function Station({ navigation }) {
+  function navigateToStationTimeTable(stationName) {
+    switch (stationName?.toLowerCase()) {
+      case 'райымбек батыра':
+        navigation.navigate('StationTimetable', {
+          name: 'Райымбек батыра',
+        });
+        break;
+      case 'жибек жолы':
+        navigation.navigate('StationTimetable', {
+          name: 'Жибек жолы',
+        });
+        break;
+      case 'алмалы':
+        navigation.navigate('StationTimetable', {
+          name: 'Алмалы',
+        });
+        break;
+      case 'абая':
+        navigation.navigate('StationTimetable', {
+          name: 'Абая',
+        });
+        break;
+      case 'байконыр':
+        navigation.navigate('StationTimetable', {
+          name: 'Байконыр',
+        });
+        break;
+      case 'театр имени мухтара ауэзова':
+        navigation.navigate('StationTimetable', {
+          name: 'Театр имени Мухтара Ауэзова',
+        });
+        break;
+      case 'алатау':
+        navigation.navigate('StationTimetable', {
+          name: 'Алатау',
+        });
+        break;
+      case 'сайран':
+        navigation.navigate('StationTimetable', {
+          name: 'Сайран',
+        });
+        break;
+      case 'москва':
+        navigation.navigate('StationTimetable', {
+          name: 'Москва',
+        });
+        break;
+      case 'сарыарка':
+        navigation.navigate('StationTimetable', {
+          name: 'Сарыарка',
+        });
+        break;
+      case 'б.момышулы':
+        navigation.navigate('StationTimetable', {
+          name: 'Б.Момышулы',
+        });
+        break;
+    }
+  }
+
   return (
-    <ScrollView>
-      <View>
-        <StationView>
-          <Pressable
-            onPress={() => {
-              navigation.navigate('StationTimetable', {
-                name: 'Райымбек батыра',
-              });
-            }}
-          >
-            <StationTitle>«Райымбек батыра»</StationTitle>
-          </Pressable>
-        </StationView>
-
-        <StationView>
-          <Pressable
-            onPress={() => {
-              navigation.navigate('StationTimetable', {
-                name: 'Жибек жолы',
-              });
-            }}
-          >
-            <StationTitle>«Жибек жолы»</StationTitle>
-          </Pressable>
-        </StationView>
-
-        <StationView>
-          <Pressable
-            onPress={() => {
-              navigation.navigate('StationTimetable', {
-                name: 'Алмалы',
-              });
-            }}
-          >
-            <StationTitle>«Алмалы»</StationTitle>
-          </Pressable>
-        </StationView>
-
-        <StationView>
-          <Pressable
-            onPress={() => {
-              navigation.navigate('StationTimetable', {
-                name: 'Абая',
-              });
-            }}
-          >
-            <StationTitle>«Абая»</StationTitle>
-          </Pressable>
-        </StationView>
-
-        <StationView>
-          <Pressable
-            onPress={() => {
-              navigation.navigate('StationTimetable', {
-                name: 'Байконыр',
-              });
-            }}
-          >
-            <StationTitle>«Байконыр»</StationTitle>
-          </Pressable>
-        </StationView>
-
-        <StationView>
-          <Pressable
-            onPress={() => {
-              navigation.navigate('StationTimetable', {
-                name: 'Театр имени Мухтара Ауэзова',
-              });
-            }}
-          >
-            <StationTitle>«Театр имени Мухтара Ауэзова»</StationTitle>
-          </Pressable>
-        </StationView>
-
-        <StationView>
-          <Pressable
-            onPress={() => {
-              navigation.navigate('StationTimetable', {
-                name: 'Алатау',
-              });
-            }}
-          >
-            <StationTitle>«Алатау»</StationTitle>
-          </Pressable>
-        </StationView>
-
-        <StationView>
-          <Pressable
-            onPress={() => {
-              navigation.navigate('StationTimetable', {
-                name: 'Сайран',
-              });
-            }}
-          >
-            <StationTitle>«Сайран»</StationTitle>
-          </Pressable>
-        </StationView>
-
-        <StationView>
-          <Pressable
-            onPress={() => {
-              navigation.navigate('StationTimetable', {
-                name: 'Москва',
-              });
-            }}
-          >
-            <StationTitle>«Москва»</StationTitle>
-          </Pressable>
-        </StationView>
-
-        <StationView>
-          <Pressable
-            onPress={() => {
-              navigation.navigate('StationTimetable', {
-                name: 'Сарыарка',
-              });
-            }}
-          >
-            <StationTitle>«Сарыарка»</StationTitle>
-          </Pressable>
-        </StationView>
-
-        <StationView>
-          <Pressable
-            onPress={() => {
-              navigation.navigate('StationTimetable', {
-                name: 'Б.Момышулы',
-              });
-            }}
-          >
-            <StationTitle>«Б.Момышулы»</StationTitle>
-          </Pressable>
-        </StationView>
-      </View>
-    </ScrollView>
+    <StationList navigateToStationTimeTable={navigateToStationTimeTable} />
   );
 }
 
